a20481c5e7b29dd07ab5d4d08dedc5f516fda692
Farid118 <faridmasjedi118@gmail.com>
2022-07-17 19:40:55 +1000

    Lesson 1 & 2 & 3

---
+++ values/test.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("2+2: ", 2+2);
+	fmt.Println("Farid " + "Masjedi");
+	fmt.Println(true || false);
+	fmt.Println(true && false);
+	fmt.Println(!false);
+}
---
+++ variables/test.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+func main(){
+	var a = "Farid"
+	var b string = "Masjedi"
+	var c = 2*2
+	var d int = 32/2
+	var f bool = false
+	var g = true
+	var e int
+	h := "Age is 37"
+
+	fmt.Println("My name is : " + a + " " + b)
+	fmt.Println(h)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(f || g)
+	fmt.Println(e)
+}
