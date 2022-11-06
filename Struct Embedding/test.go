9e949a284686385c0f36f04ed8d76c3feb429e67
Farid Masjedi <farid.masjedi1985@gmail.com>
2022-11-06 21:48:56 +1100

    Lesson 22

---
+++ Struct Embedding/test.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type base struct { 
+	num int
+}
+
+func (b base) describe() string {
+	return fmt.Sprintf("base with num = %v", b.num)
+}
+
+type container struct {
+	base
+	str string
+}
+
+func main() {
+	co := container{
+		base: base{ 
+			num: 1, 
+		},
+		str: "some name",
+	}
+
+	fmt.Println("num:", co.base.num)
+	fmt.Println("describe co:", co.describe())
+}
